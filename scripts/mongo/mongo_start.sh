1a717e05aa592c5138f21d1b0a5defac5bc2e4af
Taras <tkramarets@gmail.com>
2021-04-23 13:40:43 +0000

    fixes

--- scripts/mongo/mongo_start.sh
+++ scripts/mongo/mongo_start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
-docker-compose -f ../../docker-compose-mongo.yaml up -d && sleep 50 && docker-compose stop mongo-setup finalizer
+docker-compose -f ./docker-compose-mongo.yaml up -d && sleep 50 && docker-compose stop mongo-setup finalizer
 
 echo "Mongo started...."
--- scripts/mongo/prepare_mongo_to_first_start.sh
+++ scripts/mongo/prepare_mongo_to_first_start.sh
@@ -13,5 +13,5 @@ echo "Chown 999 for keyfile.pem:"
 chown 999:999 keyfile.pem
 
 echo "Prepare local folders:"
-mkdir -p ./data/mongo-setup/db ./data/mongo1/db ./data/mongo2/db ./data/mongo3/db ./data/mongo4/db ./data/mongo5/db ./data/mongo6/db ./data/mongo1/configdb ./data/mongo2/configdb ./data/mongo3/configdb ./data/mongo4/configdb ./data/mongo5/configdb ./data/mongo6/configdb
+mkdir -p ./data/mongo-setup/db ./data/mongo1/db ./data/mongo2/db ./data/mongo3/db ./data/mongo4/db ./data/mongo5/db ./data/mongo6/db 
 echo "Sucessfully create required folders"
